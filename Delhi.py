6529bd6d69e449403903d601c443af9016d1beed
Joe Klein <jklein694@gmail.com>
2017-07-14 21:21:03 -0400

    Mike's WebScrapper

--- Delhi.py
+++ Delhi.py
@@ -1,9 +1,13 @@
-# creating a table to store data in right format
-# get requests
-# writing a for loop to increase SRecNo=0 by 8 ever search 0,8,16,24
-from bs4 import BeautifulSoup
-import requests
 
+
+import requests
+import pandas as pd
+
+
+
+
+
+'''
 url = "http://delhihighcourt.nic.in/dhc_case_status_list_new.asp?ayear=&pyear=&SNo=1&SRecNo=0&dno=&dyear=&ctype=ALL&cno=&cyear=1980&party=&adv="
 r = requests.get(url)
 soup = BeautifulSoup(r.text)
@@ -33,13 +37,11 @@ for i in range(1, 11):
     x = requests.get(html)
     people.append(x.json())
 
-'''
+
 g_data = soup.find_all("ul", {"class": "clearfix grid"})
 oddLists = soup.find("li", {"class": "clearfix odd"})
 evenLists = soup.find("li", {"class": "clearfix even"})
-'''
 
-'''
 
 # print (g_data)
 
@@ -55,5 +57,5 @@ for item in g_data:
     #
     # print (oddLists)
     # print (evenLists)
-'''
 
+'''
